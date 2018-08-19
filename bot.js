ea62d6811fe2ba7a406f0b835f69af1b4b251876
j0shzy <42507292+j0shzy@users.noreply.github.com>
2018-08-18 22:02:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Wumpus') {
+    	message.reply('Yes I am Wumpus the almighty!!!');
   	}
 });
 
